a50074a0281ba1230a497beb0e5e409256057d8c
star0504 <sdkyu12345@gmail.com>
2023-07-30 21:54:39 +0900

    Update Landing.js

--- front/src/views/Landing.js
+++ front/src/views/Landing.js
@@ -2,27 +2,35 @@ import { React, useEffect, useState } from "react";
 import "../css/landing.css";
 import backgroundVideo from "../assets/video.mp4";
 import Login from "./Login";
+import SignUp from "./SignUp";
 const Template = (props) => {
   return (
-    <div id="landing">
-      <h1 id="landing-header-name">project name</h1>
+    <div id='landing'>
+      <h1 id='landing-header-name'>project name</h1>
       <video
         src={backgroundVideo}
         autoPlay
         loop
         muted
-        id="landing-background-video"
+        id='landing-background-video'
       />
-      <button id="login-page-button" onClick={props.isLoginHandler}>
-        login
+      <button id='login-page-button' onClick={props.isLoginHandler}>
+        Sign In
       </button>
-      {props.isLogin && <Login loginSetFalse={props.loginSetFalse} />}
+      {props.isLogin && (
+        <Login
+          loginSetFalse={props.loginSetFalse}
+          signUpSetTrue={props.signUpSetTrue}
+        />
+      )}
+      {props.isSignUp && <SignUp signUpSetFalse={props.signUpSetFalse} />}
     </div>
   );
 };
 
 const Landing = () => {
   const [isLogin, setIsLogin] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(false);
 
   const isLoginHandler = (e) => {
     e.preventDefault();
@@ -35,11 +43,25 @@ const Landing = () => {
     setIsLogin(false);
   };
 
+  const signUpSetTrue = (e) => {
+    e.preventDefault();
+    setIsSignUp(true);
+    setIsLogin(false);
+  };
+
+  const signUpSetFalse = (e) => {
+    e.preventDefault();
+    setIsSignUp(false);
+  };
+
   return (
     <Template
       isLogin={isLogin}
+      isSignUp={isSignUp}
       isLoginHandler={isLoginHandler}
       loginSetFalse={loginSetFalse}
+      signUpSetFalse={signUpSetFalse}
+      signUpSetTrue={signUpSetTrue}
     />
   );
 };
