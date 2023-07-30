54b12d39cdb793b75001df9d77030562c9a899f1
star0504 <sdkyu12345@gmail.com>
2023-07-30 21:52:29 +0900

    :sparkles: 테스트 용 서버 생성

--- demo/src/main/java/com/example/demo/controller/UserSignUp.java
+++ demo/src/main/java/com/example/demo/controller/UserSignUp.java
@@ -1,10 +1,7 @@
 package com.example.demo.controller;
 
-import org.apache.commons.logging.LogFactory;
-import org.apache.logging.log4j.spi.LoggerContextFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 
 @RestController
-public class UserLogin {
+public class UserSignUp {
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private static void accept(String key, String val) {
@@ -20,9 +17,15 @@ public class UserLogin {
     }
 
     @PostMapping("/api/login")
-    public String getLoginRequest(@RequestBody HashMap<String,String> body) {
+    public String LoginRequestHandler(@RequestBody HashMap<String,String> body) {
 
-        body.forEach(UserLogin::accept);
+        body.forEach(UserSignUp::accept);
+        return body.toString();
+    }
+
+    @PostMapping("/api/signup")
+    public String SignUpRequestHandler(@RequestBody HashMap<String,String> body) {
+        body.forEach(UserSignUp::accept);
         return body.toString();
     }
 }
