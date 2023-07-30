7c06c7af40d13a599b38095367cee70f2d0c3e0d
star0504 <sdkyu12345@gmail.com>
2023-07-30 21:52:53 +0900

    :art: proxy 설정 파일

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -2,8 +2,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("", {
-      target: "http://localhost:8080",
+    createProxyMiddleware("/api", {
+      target: "httpnpm://localhost:8080",
       changeOrigin: true,
     })
   );
