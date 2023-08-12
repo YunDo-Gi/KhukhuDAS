79ec6637c2a6aeb0169df5cb32caf55401348960
JumpWithJmc0504 <sdkyu12345@gmail.com>
2023-08-12 19:15:00 +0900

    header 포함

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,14 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import React from "react";
 import Landing from "./views/Landing";
-
+import Header from "./views/Header";
 function App() {
-  return <Landing />;
+  return (
+    <>
+      <Header />
+      <Landing />
+    </>
+  );
 }
 
 export default App;
