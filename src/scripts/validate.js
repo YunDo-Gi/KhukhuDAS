817b8a94530275070b001c798ed4cfeba74ed076
JumpWithJmc0504 <sdkyu12345@gmail.com>
2023-08-24 20:43:47 +0900

    :sparkles: Create login page

---
+++ src/scripts/validate.js
@@ -0,0 +1,42 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+  const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+
+  // If validation failed, wrapper which inside this function will be included in alertPlaceholder element.
+  const appendAlert = (message, type) => {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      `   <div>${message}</div>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      "</div>",
+    ].join("");
+
+    alertPlaceholder.append(wrapper);
+  };
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+
+          appendAlert(
+            "Something's wrong. Please check the form again.",
+            "danger"
+          );
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
