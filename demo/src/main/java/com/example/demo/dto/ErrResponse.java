9b82018bcb0a0180b330f87aac109ee1b938345c
Martin0o0 <8546088@naver.com>
2023-08-25 22:12:07 +0900

    feat: 예외 응답 DTO 생성

---
+++ demo/src/main/java/com/example/demo/dto/ErrResponse.java
@@ -0,0 +1,28 @@
+package com.example.demo.dto;
+
+import com.example.demo.exception.ErrCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+public class ErrResponse {
+
+    private String code;
+
+    private int status;
+
+    private String message;
+
+    public ErrResponse(ErrCode errCode) {
+        this.code = errCode.getCode();
+        this.message = errCode.getMessage();
+        this.code = errCode.getCode();
+    }
+
+}
