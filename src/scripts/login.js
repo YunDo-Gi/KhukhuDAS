04088ad9c25adf238f0327158dc53423b6c8eae5
jmc0504 <92536620+JeonMinCheol@users.noreply.github.com>
2023-08-28 18:21:51 +0900

    :art: 집가서 수정

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -16,15 +16,26 @@ form.addEventListener("submit", async (e) => {
     headers: {
       "Content-Type": "application/json",
     },
-  }).then((res) => {
+  }).then(async (res) => {
     if (res.headers.get("Authorization") != null) {
       localStorage.setItem("jwt", res.headers.get("Authorization"));
+
+      const reader = res.body
+      .pipeThrough(new TextDecoderStream())
+      .getReader();
+
+      while (true) {
+        const { value, done } = await reader.read();
+        if (done) break;
+        console.log(value);
+      }
+
       alert(
-        "토큰 값 :" +
-          localStorage.getItem("jwt") +
-          "\n" +
           "로그인에 성공했습니다."
       );
+
+      location.href="./index.html"
+
     } else {
       alert("이메일 또는 비밀번호가 잘못되었습니다.");
     }
