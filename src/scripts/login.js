3ef9743de4cf609a0400f9e41488a711542d082e
jmc0504 <92536620+JeonMinCheol@users.noreply.github.com>
2023-08-28 16:21:05 +0900

    Update login.js

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -25,6 +25,7 @@ form.addEventListener("submit", async (e) => {
           "\n" +
           "로그인에 성공했습니다."
       );
+      location.href="./index.html"
     } else {
       alert("이메일 또는 비밀번호가 잘못되었습니다.");
     }
