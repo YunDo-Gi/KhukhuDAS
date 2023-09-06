830b9e07f0d58eec975bdaaed57b435e8cf734ae
star0504 <sdkyu12345@gmail.com>
2023-09-07 00:08:33 +0900

    이미지를 받아오는 코드를 수정

--- src/scripts/user.js
+++ src/scripts/user.js
@@ -9,17 +9,9 @@ const profile = document.querySelector("#InputProfile");
 const button = document.querySelector(".btn-submit");
 const changeUser = document.querySelector("#ChangeUser");
 const profileContainer = document.querySelector("#ProfileContainer");
+const preview = document.querySelector("#previewImg");
 
 let flag = false;
-
-// 나이 추가 함수
-const addInputAgeValue = () => {
-  for (let i = 0; i < 100; i++) {
-    const age = document.querySelector("#InputAge");
-    age.append(new Option(i, i));
-  }
-};
-addInputAgeValue();
 
 const getUserInfo = () => {
   const url = "http://localhost:8080/api/auth/profile";
@@ -35,23 +27,26 @@ const getUserInfo = () => {
     let p = JSON.parse(value);
 
     nick.value = p.nickname;
-    password.value = p.password;
     age.value = p.age;
     email.value = p.email;
     job.value = p.job;
     phoneNumber.value = p.phoneNumber;
-    localStorage.getItem("profileImgUrl", p.profileImgUrl);
+    preview.src = "http://localhost:3000/public/default-avatar.jpg";
+    if (p.profileImgURL != null)
+      preview.src =
+        "http://localhost:8080/api/profileImg/" +
+        p.profileImgURL.replace("/profileImg\\", "");
+
     realname.value = p.realName;
     console.log(p);
   });
 
-  document.querySelector("#previewImg").src =
-    "http://localhost:3000/public/default-avatar.jpg";
-  if (localStorage.getItem("profileImgUrl") != "l") {
-    document.querySelector("#previewImg").src =
-      "http://localhost:8080/api/profileImg/" +
-      localStorage.getItem("profileImgUrl");
-  }
+  // preview.src = "http://localhost:3000/public/default-avatar.jpg";
+  // if (localStorage.getItem("profileImgUrl") != "l") {
+  //   preview.src =
+  //     "http://localhost:8080/api/profileImg/" +
+  //     localStorage.getItem("profileImgUrl");
+  // }
 };
 
 // 입장 시 데이터 받아옴
@@ -60,25 +55,25 @@ getUserInfo();
 
 const infoChangeHandler = () => {
   if (flag == true) {
-    nick.toggleAttribute("disabled");
-    password.toggleAttribute("disabled");
-    email.toggleAttribute("disabled");
-    realname.toggleAttribute("disabled");
-    job.toggleAttribute("disabled");
-    age.toggleAttribute("disabled");
-    phoneNumber.toggleAttribute("disabled");
+    // nick.toggleAttribute("disabled");
+    // password.toggleAttribute("disabled");
+    // email.toggleAttribute("disabled");
+    // realname.toggleAttribute("disabled");
+    // job.toggleAttribute("disabled");
+    // age.toggleAttribute("disabled");
+    // phoneNumber.toggleAttribute("disabled");
     changeUser.innerHTML = "정보 수정하기";
     button.style.display = "none";
     profileContainer.style.display = "none";
     flag = false;
   } else {
-    nick.removeAttribute("disabled");
-    password.removeAttribute("disabled");
-    email.removeAttribute("disabled");
-    realname.removeAttribute("disabled");
-    job.removeAttribute("disabled");
-    age.removeAttribute("disabled");
-    phoneNumber.removeAttribute("disabled");
+    // nick.removeAttribute("disabled");
+    // password.removeAttribute("disabled");
+    // email.removeAttribute("disabled");
+    // realname.removeAttribute("disabled");
+    // job.removeAttribute("disabled");
+    // age.removeAttribute("disabled");
+    // phoneNumber.removeAttribute("disabled");
     changeUser.innerHTML = "수정 취소";
     button.style.display = "block";
     profileContainer.style.display = "block";
@@ -89,19 +84,22 @@ const infoChangeHandler = () => {
 const requestHandler = () => {
   const url = "http://localhost:8080/api/auth/profile";
   let body = new FormData();
-  body.append(
-    "updateRequest",
-    JSON.stringify({
-      email: email.value,
-      job: job.options[job.selectedIndex].value,
-      realName: realname.value,
-      password: password.value,
-      age: age.options[age.selectedIndex].value,
-      nickname: nick.value,
-      phoneNumber: phoneNumber.value,
-    })
-  );
-  if (profile.files[0] != null) body.append("profileImg", profile.files[0]);
+  let data = JSON.stringify({
+    email: email.value,
+    job: job.value,
+    realName: realname.value,
+    age: age.value,
+    nickname: nick.value,
+    phoneNumber: phoneNumber.value,
+    isChangedProfileImg: false,
+  });
+
+  if (profile.files[0] != null) {
+    data.isChangedProfileImg = true;
+    body.append("profileImg", profile.files[0]);
+  }
+
+  body.append("updateRequest", data);
 
   const req = fetch(url, {
     method: "PUT",
@@ -112,6 +110,9 @@ const requestHandler = () => {
   }).then(async (res) => {
     if (res.status == 200) {
       alert("정보가 변경되었습니다.");
+      if (profile.files[0] != null)
+        localStorage.setItem("profileImgUrl", profile.files[0]);
+      getUserInfo();
     } else {
       alert("사용자 정보를 변경할 수 없습니다.");
     }
