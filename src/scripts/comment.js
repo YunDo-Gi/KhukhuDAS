8d8b5bba50fd5af8465d88044b4b0cfe3d32a309
jmc0504 <92536620+JeonMinCheol@users.noreply.github.com>
2023-09-08 12:53:38 +0900

    댓글 창 폼 구현

--- src/scripts/comment.js
+++ src/scripts/comment.js
@@ -1,11 +1,21 @@
+const input = document.querySelector("#comment-input")
+const btn = document.querySelector("#comment-btn")
+
+// 나중에 방 만들면 사용 예정
+// const roomId = localStorage.getItem("roomId")
+
+console.log(localStorage.getItem("jwt"))
+
 const createComment = async (roomId) => {
-    let url = `http://localhost:8080/api/room/${roomId}/comment`;
-    ;
+  let url = `http://localhost:8080/api/room/${roomId}/comment`;
+
+  let comment = input.value;
+  if(comment != null) {
     try {
       let res = await fetch(url, {
         method: "POST",
         body:JSON.stringify({
-          content : "test?"
+          content : input.value
         }),
         headers: {
           Authorization: "Bearer " + localStorage.getItem("jwt"),
@@ -15,29 +25,67 @@ const createComment = async (roomId) => {
     } catch (e) {
       console.log(e);
     }
+  } else {
+    alert("댓글이 입력되지 않았습니다.")
   }
   
-  const updateComment = async (roomId, commentId) => {
-    let url = `http://localhost:8080/api/room/${roomId}/comment/${commentId}`;
-    let roomCommentRequest = "change";
-    try {
-      let res = await fetch(url, {
-        method: "PUT",
-        body:JSON.stringify({
-          content : "change"
-        }),
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("jwt"),
-          "Content-Type": "application/json"
-        },
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }
+}
 
-  // test
-  room.addEventListener("click", () => {
-    // createComment(1)
-    // updateComment(1,1);
-  });
+const updateComment = async (roomId, commentId) => {
+  let url = `http://localhost:8080/api/room/${roomId}/comment/${commentId}`;
+  try {
+    let res = await fetch(url, {
+      method: "PUT",
+      body:JSON.stringify({
+        content : input.value
+      }),
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+        "Content-Type": "application/json"
+      },
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const createRecomment = async (roomId, commentId) => {
+  let url = `http://localhost:8080/api/room/${roomId}/comment/${commentId}/recomment`;
+  try {
+    let res = await fetch(url, {
+      method: "POST",
+      body:JSON.stringify({
+        content : input.value
+      }),
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+        "Content-Type": "application/json"
+      },
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const updateRecomment = async (roomId, commentId, recommentId) => {
+  let url = `http://localhost:8080/api/room/${roomId}/comment/${commentId}/recomment/${recommentId}`;
+  try {
+    let res = await fetch(url, {
+      method: "PUT",
+      body:JSON.stringify({
+        content : input.value
+      }),
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+        "Content-Type": "application/json"
+      },
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// test
+btn.addEventListener("click", (roomId)=>{
+  createRecomment(1,1);
+})
