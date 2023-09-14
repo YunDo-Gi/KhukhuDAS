7e2a1497ad46e4c590468a2185ea9cf7403d5b47
jmc0504 <92536620+JeonMinCheol@users.noreply.github.com>
2023-09-14 16:12:30 +0900

    Create method : getRoom, deleteRoom

--- src/scripts/room.js
+++ src/scripts/room.js
@@ -1,7 +1,9 @@
 const room = document.querySelector(".get-room-test");
 console.log(localStorage.getItem("jwt"))
-const getRoom = async (roomId) => {
+
+const getRoom = async (roomId) => { // 방 입장
   let url = `http://localhost:8080/api/room/${roomId}`;
+  localStorage.setItem("roomId", roomId) // id 저장, 방 수정 시 활용
 
   try {
     let res = await fetch(url, {
@@ -18,6 +20,21 @@ const getRoom = async (roomId) => {
     console.log(e);
   }
 };
+
+const getRooms = async (interestType, sort) => { // 메인 페이지에 보여줄 거임
+  // interest가 null인 경우 구분 안함
+  // sort는 3가지 종류로 구성
+  // 1. LIKE
+  // 2. VIEW
+  // 3. CHRONOLOGICAL
+  const url = (interestType == null) ? `http://localhost:8080/api/rooms?sort=${sort}` : `http://localhost:8080/api/rooms?interest-type=${interestType}&sort=${sort}`;
+  
+  let res = await fetch(url, );
+  const reader = res.body.pipeThrough(new TextDecoderStream()).getReader();
+  const { value } = await reader.read();
+  
+  console.log(JSON.parse(value));
+}
 
 const likeThisRoom = async (roomId) => {
   let url = `http://localhost:8080/api/room/${roomId}/like`;
@@ -43,6 +60,27 @@ const unlikeThisRoom = async (roomId) => {
   });
   console.log(res.status)
 }
+
+const deleteRoom = async (roomId) => { // 방 입장
+  let url = `http://localhost:8080/api/room/${roomId}`;
+  localStorage.setItem("roomId", roomId) // id 저장, 방 수정 시 활용
+
+  try {
+    let res = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+    });
+
+    const reader = res.body.pipeThrough(new TextDecoderStream()).getReader();
+    const { value } = await reader.read();
+    console.log(JSON.parse(value));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 room.addEventListener("click", async () => {
   // getRoom(1);
   await unlikeThisRoom(1);
