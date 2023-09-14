381062ab1c102500d3ab3f85cb96d93739e252a6
jmc0504 <92536620+JeonMinCheol@users.noreply.github.com>
2023-09-14 15:16:19 +0900

    :art: 이미지 변경 시 사용자에게 보이도록 수정

--- src/scripts/user.js
+++ src/scripts/user.js
@@ -9,9 +9,23 @@ const profile = document.querySelector("#InputProfile");
 const button = document.querySelector(".btn-submit");
 const changeUser = document.querySelector("#ChangeUser");
 const profileContainer = document.querySelector("#ProfileContainer");
-const preview = document.querySelector("#previewImg");
-
+const profileImg = document.querySelector("#profileImg");
+let imgBuffer = null;
 let flag = false;
+
+// 이미지 URL을 받아와서 보여주는 함수
+const handleFiles = () => {
+  const selectedFile = profile.files[0];
+  const fileReader = new FileReader();
+
+  fileReader.readAsDataURL(selectedFile);
+
+  fileReader.onload = function () {
+    profileImg.src = fileReader.result;
+  };
+
+  return selectedFile;
+};
 
 const getUserInfo = () => {
   const url = "http://localhost:8080/api/auth/profile";
@@ -31,22 +45,12 @@ const getUserInfo = () => {
     email.value = p.email;
     job.value = p.job;
     phoneNumber.value = p.phoneNumber;
-    preview.src = "../../public/default-avatar.jpg";
-
-    if (p.profileImgURL != null)
-      preview.src =
-        "http://localhost:8080/api/profileImg/" +
-        p.profileImgURL.replace("\\\\profileImg\\", "");
-
+    imgBuffer  = "../../public/default-avatar.jpg"
+    if (p.profileImgURL != null) imgBuffer = "http://localhost:8080/api/profileImg/" + p.profileImgURL.replace("\\\\profileImg\\", "");
+  
+    profileImg.src = imgBuffer;
     realname.value = p.realName;
   });
-
-  // preview.src = "http://localhost:3000/public/default-avatar.jpg";
-  // if (localStorage.getItem("profileImgUrl") != "l") {
-  //   preview.src =
-  //     "http://localhost:8080/api/profileImg/" +
-  //     localStorage.getItem("profileImgUrl");
-  // }
 };
 
 // 입장 시 데이터 받아옴
@@ -55,25 +59,13 @@ getUserInfo();
 
 const infoChangeHandler = () => {
   if (flag == true) {
-    // nick.toggleAttribute("disabled");
-    // password.toggleAttribute("disabled");
-    // email.toggleAttribute("disabled");
-    // realname.toggleAttribute("disabled");
-    // job.toggleAttribute("disabled");
-    // age.toggleAttribute("disabled");
-    // phoneNumber.toggleAttribute("disabled");
     changeUser.innerHTML = "정보 수정하기";
     button.style.display = "none";
     profileContainer.style.display = "none";
     flag = false;
+    profile.value = null
+    getUserInfo();
   } else {
-    // nick.removeAttribute("disabled");
-    // password.removeAttribute("disabled");
-    // email.removeAttribute("disabled");
-    // realname.removeAttribute("disabled");
-    // job.removeAttribute("disabled");
-    // age.removeAttribute("disabled");
-    // phoneNumber.removeAttribute("disabled");
     changeUser.innerHTML = "수정 취소";
     button.style.display = "block";
     profileContainer.style.display = "block";
@@ -134,3 +126,4 @@ const requestHandler = () => {
 
 changeUser.addEventListener("click", infoChangeHandler);
 button.addEventListener("click", requestHandler);
+profile.addEventListener("change", handleFiles);
